b5b2443ddbfde95d3fd8c4a9ddcdac16aa597a61
Mateo Filip <mateofernandofilip@gmail.com>
2022-06-02 16:19:43 -0300

    Now it, technically, works. Styling and details left

--- src/App.js
+++ src/App.js
@@ -1,13 +1,50 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import Body from './components/Body/Body.jsx';
 import Navbar from './components/Navbar/Navbar.jsx';
 import './App.scss';
 import 'animate.css';
 
+const apiKey = '95ec01f8b61f542bd3d75bc4a0bf4394';
+
 export default function App() {
+  const [longitude, setLongitude] = useState('');
+  const [latitude, setLatitude] = useState('');
+  const [weather, setWeather] = useState([]);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setLongitude(position.coords.longitude);
+      setLatitude(position.coords.latitude);
+    });
+
+    getWeather(latitude, longitude);
+  }, [latitude, longitude]);
+
+  async function getWeather(latitude, longitude) {
+    let initialData = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${apiKey}`
+    );
+
+    let weatherData = initialData.data;
+
+    const weather = {
+      city: weatherData.name,
+      temp: Math.round(weatherData.main.temp),
+      maxTemp: Math.round(weatherData.main.temp_max),
+      minTemp: Math.round(weatherData.main.temp_min),
+      feelsLike: Math.round(weatherData.main.feels_like),
+      weather: weatherData.weather[0].main,
+    };
+
+    setWeather(weather);
+    console.log(weather);
+  }
+
   return (
     <div className="App">
       <Navbar />
-      <Body />
+      <Body weather={weather} />
     </div>
   );
 }
--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,9 +1,16 @@
 import './Body.scss';
 
-export default function Body() {
+export default function Body(props) {
   return (
     <div className="Body">
-      <h1>30ºC</h1>
+      <h1>
+        {props.weather.city} - {props.weather.weather}
+      </h1>
+      <h2>{props.weather.temp}&deg;C</h2>
+      <h3>
+        {props.weather.maxTemp}&deg;C | {props.weather.minTemp}&deg;C
+      </h3>
+      <h4>Feels Like {props.weather.feelsLike}&deg;C</h4>
     </div>
   );
 }
